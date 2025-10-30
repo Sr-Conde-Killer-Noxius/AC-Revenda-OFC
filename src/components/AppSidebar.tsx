ac99d39ddde49686ab831a71eaa724c9375de511
Sr. Noxius CyberWork <noxius.cyberwork@gmail.com>
2025-10-30 20:00:06 -0300

    Updated src/components/AppSidebar.tsx

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -64,7 +64,7 @@ export function AppSidebar() {
           <div>
             <h2 className="text-sm font-semibold text-sidebar-foreground">Painel Revenda</h2>
             <p className="text-xs text-muted-foreground">Acerto Certo</p>
-            <p className="text-xs text-muted-foreground">1.0.0.1.0</p>
+            <p className="text-xs text-muted-foreground">1.0.2.0.0</p>
           </div>
         </div>
       </SidebarHeader>
